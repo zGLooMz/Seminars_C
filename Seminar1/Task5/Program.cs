0f4188a97f191dfe1bd9c18f369eed3d8454d73e
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-10-13 23:55:31 +0300

    Ad Seminar1\Task5

---
+++ Seminar1/Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// *. Напишите программу вычисления модуля числа.
+// 	 2 -> 2
+// 	-3 -> 3
+// 	-7 -> 7
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int mod = number;
+if(number<0) 
+    mod = -number;
+
+Console.WriteLine($"{mod} модуль числа {number}");
