622a28b53454fa76914d30d55ddff2194108a518
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-10-13 23:11:00 +0300

    Ad Seminar1\Task3

---
+++ Seminar1/Task3/Program.cs
@@ -0,0 +1,7 @@
+﻿// 3. Напишите программу вычисления функции:
+// x = f(a)  -  функция куба
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int x = a*a*a;
+Console.WriteLine(x);
