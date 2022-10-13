9a9695cf74b22d290452412f586a2de0c86643a9
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-10-14 01:14:20 +0300

    Ad Seminar1\Home4

---
+++ Seminar1/Home4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает 
+// число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if(N<=0)
+{
+    int count = 0;
+    while (count >= N)
+    {
+        Console.Write($"{count}, ");
+        count = count -2;
+    }
+}
+else
+{
+    int count = 2;
+    while (count <= N)
+    {
+        Console.Write($"{count}, ");
+        count = count + 2;
+    } 
+}
