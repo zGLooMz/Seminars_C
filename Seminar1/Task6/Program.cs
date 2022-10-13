98a3a7efac6891a0e0109e13d686684a9811aef4
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-10-14 00:07:30 +0300

    Ad Seminar1\Task6

---
+++ Seminar1/Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿// **. Напишите программу, которая на вход принимает значение, а  на выходе показывает обратное значение.
+// 1 -> 1 
+// 2 -> 0.5
+// 0.25 -> 4
+
+System.Console.Write("Введите число: ");
+double number = Convert.ToDouble(Console.ReadLine());
+double back = 1/number;
+Console.WriteLine($"{back:f2}");
