566cd3ea70406337438c9beb683fcdcd5801f705
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-10-17 23:24:13 +0300

    Add Seminar2\Home2

---
+++ Seminar2/Home2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное 
+// число и удаляет вторую цифру этого числа. Не использовать строки для расчета.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+int firstDigit = (number / 100) % 10;
+int lastDigit = number % 10;
+
+
+Console.WriteLine($"{number} -> {firstDigit}{lastDigit}");
