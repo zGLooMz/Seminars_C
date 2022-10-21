a98c57eb17c097a346c70c0fa177b5a945a04012
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-10-21 21:27:22 +0300

    Add Seminar3\Home3

---
+++ Seminar3/Home3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27. 
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+int number = Prompt("Введите число больше 0: ");
+
+for (int i = 1; i <= number; i++) 
+{
+    Console.Write("{0}  ", Math.Pow(i, 3));
+}
