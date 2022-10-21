6ebf189a19edaa06dbd727967880c0bc006bb2ed
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-10-21 19:03:55 +0300

    Add Seminar3\Task5

---
+++ Seminar3/Task5/Program.cs
@@ -0,0 +1,49 @@
+﻿//  Напишите программу, которая генерирует последовательность случайных чисел из 10 элементов
+// в диапазоне от 1 до 10, и подсчитывает, сколько сгенерировалось чисел больше 5.
+
+
+int[] GetArr(int len)
+{
+    int[] numbers = new int[len];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1, 11);
+    }
+
+    return numbers;
+}
+
+int GetCount(int[] numbers)
+{
+    int counter = 0;
+    foreach (int item in numbers)
+    {
+        if (item > 5)
+        {
+            counter++;
+        }
+    }
+
+    return counter;
+}
+
+void PrintArr(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        System.Console.Write($"{numbers[i]} \t");
+    }
+    System.Console.WriteLine();
+}
+
+
+void Runner()
+{
+    int[] numbers = GetArr(10);
+
+    PrintArr(numbers);
+    System.Console.WriteLine($"Result: {GetCount(numbers)}");
+}
+
+
+Runner();
