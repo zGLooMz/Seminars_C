c761f75ed58f974f82ac9ac9b90e6efec6b6af41
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-11-09 17:52:05 +0300

    Add Seminar4\Home2

---
+++ Seminar4/Home2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе. Реализовать через функции.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int NumberSum(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+      sum = sum + number % 10;
+      number = number / 10;
+    }
+   return sum;
+}
+
+int number = Prompt("Введите число: ");
+int sumNumber = NumberSum(number);
+Console.WriteLine($"Сумма цифр в числе: {sumNumber}");
