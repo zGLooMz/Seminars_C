f232a92381aaf99f0e61cd0c32ff2cd4801647bb
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-11-09 16:21:55 +0300

    Add Seminar4\Task2

---
+++ Seminar4/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+int Fact(int number)
+{
+    int factor = 1;
+    for (int i = 2; i <= number; i++)
+    {
+        factor = factor * i;
+    }
+    return factor;
+}
+int number = Prompt("Please type a number >> ");
+int factor = Fact(number);
+Console.WriteLine($"Factorial is {factor} ");
