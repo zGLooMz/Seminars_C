02107aa6f4c877c37560c5db7c4221bff92ac1c5
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-11-09 16:49:41 +0300

    Add Seminar4\Task4

---
+++ Seminar4/Task4/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу, которая из массива случайных чисел. Ищет второй максимум 
+// (число меньше максимального элемента, но больше всех остальных). Постарайтесь сделать одним циклом
+// [1, 3, 5, 6, 6, 4] -> 5
+
+// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+int[] CreateArray(int Length = 8)
+{
+    int[] tempArray = new int[Length];
+    for (int i = 0; i < Length; i++)
+    {
+        tempArray[i] = new Random().Next(1, 10);
+    }
+    return tempArray;
+}
+
+void printArray(int[] intArray)
+{
+    System.Console.Write($"{intArray[0]}");
+    for (int i = 1; i < intArray.Length; i++)
+    {
+        System.Console.Write($", {intArray[i]}");
+    }
+}
+int findSecondMax(int[] intArray)
+{
+    int tempMax = intArray[0];
+    int tempSecMax = 0;
+    for (int i = 1; i < intArray.Length; i++)
+    {
+        if (tempMax < intArray[i])
+        {
+            tempSecMax = tempMax;
+            tempMax = intArray[i];
+        }
+        else
+        {
+            if (tempSecMax <= intArray[i] && intArray[i] != tempMax)
+            {
+                tempSecMax = intArray[i];
+            }
+        }
+    }
+    return tempSecMax;
+}
+
+System.Console.Write("Случайный массив длиной 8 со значениями 0 и 1: ");
+int[] maxArray = CreateArray();
+printArray(maxArray);
+System.Console.Write($"-> {findSecondMax(maxArray)}");
