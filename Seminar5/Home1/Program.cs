b0934e2dd79a5b0f0b9ca3e83c3cdf2bef3c2b5a
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-11-13 14:16:03 +0300

    Add Seminar5\Home1

---
+++ Seminar5/Home1/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int Prompt(string strMessage)
+{
+    Console.Write(strMessage);
+    int temp = int.Parse(Console.ReadLine());
+    return temp;
+}
+
+int[] CreateArray(int length, int min, int max)
+{
+    int[] tempArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        tempArray[i] = new Random().Next(min, max);
+    }
+    return tempArray;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write($"{array[0]}");
+    for (int i = 1; i < array.Length; i++)
+    {
+        Console.Write($", {array[i]}");
+    }
+
+}
+
+int QuantityPositive(int[] array)
+{
+    int quantity = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            quantity++;
+        }
+    }
+    return quantity;
+}
+int len = Prompt("Введите размерность массива: ");
+int min = 100;
+int max = 1000;
+int[] array = CreateArray(len, min, max);
+PrintArray(array);
+Console.WriteLine();
+int quantity = QuantityPositive(array);
+Console.WriteLine($"Количество чётных чисел в массиве: {quantity}");
