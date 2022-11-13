5110e519440e524ee627072a175e125425f2907a
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-11-13 16:30:51 +0300

    Add Seminar5\Task3

---
+++ Seminar5/Task3/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 2: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int PromptInt(string strMessage)
+{
+    System.Console.Write(strMessage);
+    int temp = int.Parse(System.Console.ReadLine());
+    return temp;
+}
+int[] CreateArray(int length)
+{
+    int[] tempArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        tempArray[i] = new Random().Next(-9, 10);
+    }
+    return tempArray;
+}
+void PrintArray(int[] intArray)
+{
+    System.Console.Write($"{intArray[0]}");
+    for (int i = 1; i < intArray.Length; i++)
+    {
+        System.Console.Write($", {intArray[i]}");
+    }
+    System.Console.WriteLine();
+}
+bool IsHasNumber(int number, int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+int[] array = CreateArray(10);
+PrintArray(array);
+int number = PromptInt($" Введите число, которое ищем ");
+if (IsHasNumber(number, array))
+{
+    System.Console.WriteLine($"yes");
+}
+else
+{
+    System.Console.WriteLine($"no");
+}
