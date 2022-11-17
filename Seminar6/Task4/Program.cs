1515296d1897538958a56e52107a5a5310da9671
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-11-17 15:03:55 +0300

    Add Seminar6\Task4

---
+++ Seminar6/Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 4: Напишите программу, которая будет создавать 
+// копию заданного массива с помощью поэлементного копирования.
+
+int[] CopyArr(int[] arr)
+{
+    int[] result = new int[arr.Length];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        result[i] = arr[i];
+    }
+
+    return result;
+}
+
+// void Runner()
+// {
+//     int[] arr1 = CreateArray(len:5);
+//     PrintArray(arr1);
+
+//     int[] arr2 = arr1;
+//     PrintArray(arr2);
+
+//     arr1[0] = 422;
+//     PrintArray(arr1);
+//     PrintArray(arr2);
+
+//     int[] arr3 = CopyArr(arr1);
+//     arr1[1] = 1000;
+
+//     PrintArray(arr1);
+//     PrintArray(arr3);
+// }
+
+// Runner();
