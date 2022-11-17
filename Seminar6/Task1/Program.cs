88c8579db7094c68cfa81eb06b421e2cbb575132
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-11-17 15:03:20 +0300

    Add Seminar6\Task1

---
+++ Seminar6/Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая перевернёт одномерный массив 
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] CreateArray(int len)
+{
+    int[] result = new int[len];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(1, 10);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+}
+
+void SwapElements(int[] array, int i1, int i2)
+{
+    int temp = array[i1];
+    array[i1] = array[i2];
+    array[i2] = temp;
+}
+
+int[] ReverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length/2; i++)
+    {
+        SwapElements(array, i, array.Length - 1 - i);
+    }
+    return array;
+}
+
+int[] arr = CreateArray(10);
+PrintArray(arr);
+PrintArray(ReverseArray(arr));
