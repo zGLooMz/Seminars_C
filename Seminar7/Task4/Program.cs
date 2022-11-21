18845995cc55382b18c4ba2bd0a65979a0af6842
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-11-21 10:58:49 +0300

    Add Seminar7\Task4

---
+++ Seminar7/Task4/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 4: Задайте двумерный массив. Введите элемент, и найдите первое его вхождение,
+// выведите позиции по горизонтали и вертикали, или напишите, что такого элемента нет.
+// Например, такой массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Введенный элемент 2, результат: [1, 4]
+// Введенный элемент 6, результат: такого элемента нет.
+int Prompt(string msg)
+{
+    Console.WriteLine(msg);
+    int i = Convert.ToInt32(Console.ReadLine());
+    return i;
+}
+
+int[,] CreateArray(int m, int n)
+{
+    int[,] table = new int[m, n];
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = new Random().Next(-9, 9);
+        }
+    }
+    return table;
+}
+
+void FindElement (int[,] array, int number)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i,j] == number)
+            {
+                Console.WriteLine($"the element {number} exists, position [{i}, {j}] ");
+                return;
+            }
+            
+        }
+    }
+    Console.WriteLine($"The number was not found in the array");
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+            
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void Execute()
+{
+    int rows = Prompt("Enter quantity of rows ");
+    int columns = Prompt("Enter quantity of columns ");
+    int[,] array = CreateArray(rows, columns);
+    PrintArray(array);
+    Console.WriteLine();
+    int num = Prompt("Enter some number to find in array ");
+    FindElement(array, num);
+}
+Execute();
