57bc1ecc755f673dbab3dbccd0dfa334629af666
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-11-21 11:19:53 +0300

    Add Seminar7\Home1

---
+++ Seminar7/Home1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 1: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5  7    -2   -0,2
+// 1   -3,3   8   -9,9
+// 8    7,8  -7,1  9
+
+int PromptInt(string strMessage)
+{
+    Console.Write(strMessage);
+    int temp = int.Parse(Console.ReadLine());
+    return temp;
+}
+
+double[,] CreateMatrixDouble(int m, int n)
+{
+    double[,] array = new double[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble() * 20 - 5;
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]:f1}\t");
+        }
+        Console.Write("\n");
+    }
+}
+
+int m = PromptInt("Введите m: ");
+int n = PromptInt("Введите n: ");
+Console.WriteLine();
+double[,] matrix = CreateMatrixDouble(m, n);
+PrintMatrix(matrix);
