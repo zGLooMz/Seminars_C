8cf21316929619fed92fca1b878c68b3d36ed9c1
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-11-21 10:53:55 +0300

    Add Seminar7\Task2

---
+++ Seminar7/Task2/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 2: Задайте двумерный массив. Найдите элементы, у которых обе позиции чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int[,] CreateArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+int[,] GetNewArray(int[,] array)
+{
+    for (int i = 1; i < array.GetLength(0); i += 2)
+    {
+        for (int j = 1; j < array.GetLength(1); j += 2)
+        {
+            array[i, j] *= array[i, j];
+        }
+    }
+    return array;
+}
+
+void Execute()
+{
+    int m = ReadData("Input amount of rows");
+    int n = ReadData("Input amount of columns");
+    int[,] array = CreateArray(m, n);
+    PrintArray(array);
+    int[,] newArray = GetNewArray(array);
+    PrintArray(newArray);
+}
+
+Execute();
+
