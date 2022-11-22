2474994dc7f36ac70697f64cff6502ad188cbdb8
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-11-22 20:10:23 +0300

    Add Seminar8\Home1

---
+++ Seminar8/Home1/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 1: Задайте двумерный массив. Напишите программу, 
+// которая упорядочивает по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int Prompt(string msg)
+{
+    Console.WriteLine(msg);
+    int userInput = Convert.ToInt32(Console.ReadLine());
+    return userInput;
+}
+
+int[,] CreateArray(int rows, int columns, int lowerLim, int upperLim)
+{
+    int[,] table = new int[rows, columns];
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = new Random().Next(lowerLim, upperLim);
+        }
+    }
+    return table;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void OrderDescending(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int rows = Prompt("Enter quantity of raws ");
+int columns = Prompt("Enter quantity of columns ");
+int lowLim = Prompt("Enter lower limit for array element value");
+int upperLim = Prompt("Enter upper limit for array element value");
+int[,] baseArray = CreateArray(rows, columns, lowLim, upperLim);
+PrintArray(baseArray);
+OrderDescending(baseArray);
+PrintArray(baseArray);
